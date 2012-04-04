f4e0c4b9676000f59f88f39077cc108789de5aa2
Mouse <mouse@Rodents-Montreal.ORG>
2012-04-04 14:29:39 -0400

    Use the correct size in NFS code which works with NFSKERBKEY_T.
    NFSKERBKEY_T is an array type, so, when it's a procedure formal,
    sizeof() returns the size of a pointer rather than what we want.

--- sys/nfs/nfs_syscalls.c
+++ sys/nfs/nfs_syscalls.c
@@ -1014,7 +1014,7 @@ nfs_getauth(nmp, rep, cred, auth_str, auth_len, verf_str, verf_len, key)
 	else {
 		*auth_len = nmp->nm_authlen;
 		*verf_len = nmp->nm_verflen;
-		memcpy((caddr_t)key, (caddr_t)nmp->nm_key, sizeof (key));
+		memcpy(key, nmp->nm_key, sizeof (NFSKERBKEY_T));
 	}
 	nmp->nm_iflag &= ~NFSMNT_HASAUTH;
 	nmp->nm_iflag |= NFSMNT_WAITAUTH;
@@ -1152,7 +1152,7 @@ nfs_savenickauth(nmp, cred, len, key, mdp, dposp, mrep)
 			nuidp->nu_expire = time.tv_sec + NFS_KERBTTL;
 			nuidp->nu_timestamp = ktvout;
 			nuidp->nu_nickname = nick;
-			memcpy(nuidp->nu_key, key, sizeof (key));
+			memcpy(nuidp->nu_key, key, sizeof (NFSKERBKEY_T));
 			TAILQ_INSERT_TAIL(&nmp->nm_uidlruhead, nuidp,
 				nu_lru);
 			LIST_INSERT_HEAD(NMUIDHASH(nmp, cred->cr_uid),
