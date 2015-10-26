56940e5929db550a82bdbcf9af0d1cc617030588
Mouse <mouse@Rodents-Montreal.ORG>
2015-10-26 13:37:17 -0400

    rogue: align lists of constants better.  No semantic changes.

--- games/rogue/rogue.h
+++ games/rogue/rogue.h
@@ -74,86 +74,86 @@
 #define AMULET		((unsigned short) 0400)
 #define ALL_OBJECTS	((unsigned short) 0777)
 
-#define LEATHER 0
+#define LEATHER  0
 #define RINGMAIL 1
-#define SCALE 2
-#define CHAIN 3
-#define BANDED 4
-#define SPLINT 5
-#define PLATE 6
-#define ARMORS 7
+#define SCALE    2
+#define CHAIN    3
+#define BANDED   4
+#define SPLINT   5
+#define PLATE    6
+#define ARMORS   7
 
-#define BOW 0
-#define DART 1
-#define ARROW 2
-#define DAGGER 3
-#define SHURIKEN 4
-#define MACE 5
-#define LONG_SWORD 6
+#define BOW              0
+#define DART             1
+#define ARROW            2
+#define DAGGER           3
+#define SHURIKEN         4
+#define MACE             5
+#define LONG_SWORD       6
 #define TWO_HANDED_SWORD 7
-#define WEAPONS 8
+#define WEAPONS          8
 
 #define MAX_PACK_COUNT 24
 
-#define PROTECT_ARMOR 0
-#define HOLD_MONSTER 1
-#define ENCH_WEAPON 2
-#define ENCH_ARMOR 3
-#define IDENTIFY 4
-#define TELEPORT 5
-#define SLEEP 6
-#define SCARE_MONSTER 7
-#define REMOVE_CURSE 8
-#define CREATE_MONSTER 9
+#define PROTECT_ARMOR      0
+#define HOLD_MONSTER       1
+#define ENCH_WEAPON        2
+#define ENCH_ARMOR         3
+#define IDENTIFY           4
+#define TELEPORT           5
+#define SLEEP              6
+#define SCARE_MONSTER      7
+#define REMOVE_CURSE       8
+#define CREATE_MONSTER     9
 #define AGGRAVATE_MONSTER 10
-#define MAGIC_MAPPING 11
-#define CON_MON 12
-#define SCROLS 13
+#define MAGIC_MAPPING     11
+#define CON_MON           12
+#define SCROLS            13
 
-#define INCREASE_STRENGTH 0
-#define RESTORE_STRENGTH 1
-#define HEALING 2
-#define EXTRA_HEALING 3
-#define POISON 4
-#define RAISE_LEVEL 5
-#define BLINDNESS 6
-#define HALLUCINATION 7
-#define DETECT_MONSTER 8
-#define DETECT_OBJECTS 9
-#define CONFUSION 10
-#define LEVITATION 11
-#define HASTE_SELF 12
-#define SEE_INVISIBLE 13
-#define POTIONS 14
+#define INCREASE_STRENGTH  0
+#define RESTORE_STRENGTH   1
+#define HEALING            2
+#define EXTRA_HEALING      3
+#define POISON             4
+#define RAISE_LEVEL        5
+#define BLINDNESS          6
+#define HALLUCINATION      7
+#define DETECT_MONSTER     8
+#define DETECT_OBJECTS     9
+#define CONFUSION         10
+#define LEVITATION        11
+#define HASTE_SELF        12
+#define SEE_INVISIBLE     13
+#define POTIONS           14
 
-#define TELE_AWAY 0
-#define SLOW_MONSTER 1
-#define INVISIBILITY 2
-#define POLYMORPH 3
-#define HASTE_MONSTER 4
-#define MAGIC_MISSILE 5
-#define CANCELLATION 6
-#define DO_NOTHING 7
-#define DRAIN_LIFE 8
-#define COLD 9
-#define FIRE 10
-#define WANDS 11
+#define TELE_AWAY      0
+#define SLOW_MONSTER   1
+#define INVISIBILITY   2
+#define POLYMORPH      3
+#define HASTE_MONSTER  4
+#define MAGIC_MISSILE  5
+#define CANCELLATION   6
+#define DO_NOTHING     7
+#define DRAIN_LIFE     8
+#define COLD           9
+#define FIRE          10
+#define WANDS         11
 
-#define STEALTH 0
-#define R_TELEPORT 1
-#define REGENERATION 2
-#define SLOW_DIGEST 3
-#define ADD_STRENGTH 4
-#define SUSTAIN_STRENGTH 5
-#define DEXTERITY 6
-#define ADORNMENT 7
-#define R_SEE_INVISIBLE 8
-#define MAINTAIN_ARMOR 9
-#define SEARCHING 10
-#define RINGS 11
+#define STEALTH           0
+#define R_TELEPORT        1
+#define REGENERATION      2
+#define SLOW_DIGEST       3
+#define ADD_STRENGTH      4
+#define SUSTAIN_STRENGTH  5
+#define DEXTERITY         6
+#define ADORNMENT         7
+#define R_SEE_INVISIBLE   8
+#define MAINTAIN_ARMOR    9
+#define SEARCHING        10
+#define RINGS            11
 
 #define RATION 0
-#define FRUIT 1
+#define FRUIT  1
 
 #define NOT_USED	((unsigned short)   0)
 #define BEING_WIELDED	((unsigned short)  01)
@@ -163,14 +163,14 @@
 #define ON_EITHER_HAND	((unsigned short) 014)
 #define BEING_USED	((unsigned short) 017)
 
-#define NO_TRAP -1
-#define TRAP_DOOR 0
-#define BEAR_TRAP 1
-#define TELE_TRAP 2
-#define DART_TRAP 3
-#define SLEEPING_GAS_TRAP 4
-#define RUST_TRAP 5
-#define TRAPS 6
+#define NO_TRAP           -1
+#define TRAP_DOOR          0
+#define BEAR_TRAP          1
+#define TELE_TRAP          2
+#define DART_TRAP          3
+#define SLEEPING_GAS_TRAP  4
+#define RUST_TRAP          5
+#define TRAPS              6
 
 #define STEALTH_FACTOR 3
 #define R_TELE_PERCENT 8
@@ -316,15 +316,15 @@ typedef struct rm room;
 #define MAX_STRENGTH 99
 #define LAST_DUNGEON 99
 
-#define STAT_LEVEL 01
-#define STAT_GOLD 02
-#define STAT_HP 04
-#define STAT_STRENGTH 010
-#define STAT_ARMOR 020
-#define STAT_EXP 040
-#define STAT_HUNGER 0100
-#define STAT_LABEL 0200
-#define STAT_ALL 0377
+#define STAT_LEVEL      01
+#define STAT_GOLD       02
+#define STAT_HP         04
+#define STAT_STRENGTH  010
+#define STAT_ARMOR     020
+#define STAT_EXP       040
+#define STAT_HUNGER   0100
+#define STAT_LABEL    0200
+#define STAT_ALL      0377
 
 #define PARTY_TIME 10	/* one party somewhere in each 10 level span */
 
@@ -357,33 +357,33 @@ extern object level_monsters;
 
 #define MONSTERS 26
 
-#define HASTED					01L
-#define SLOWED					02L
-#define INVISIBLE				04L
-#define ASLEEP				   010L
-#define WAKENS				   020L
-#define WANDERS				   040L
-#define FLIES				  0100L
-#define FLITS				  0200L
-#define CAN_FLIT			  0400L		/* can, but usually doesn't, flit */
-#define CONFUSED	 		 01000L
-#define RUSTS				 02000L
-#define HOLDS				 04000L
-#define FREEZES				010000L
-#define STEALS_GOLD			020000L
-#define STEALS_ITEM			040000L
-#define STINGS			   0100000L
-#define DRAINS_LIFE		   0200000L
-#define DROPS_LEVEL		   0400000L
-#define SEEKS_GOLD		  01000000L
-#define FREEZING_ROGUE	  02000000L
-#define RUST_VANISHED	  04000000L
-#define CONFUSES		 010000000L
-#define IMITATES		 020000000L
-#define FLAMES			 040000000L
-#define STATIONARY		0100000000L		/* damage will be 1,2,3,... */
-#define NAPPING			0200000000L		/* can't wake up for a while */
-#define ALREADY_MOVED	0400000000L
+#define HASTED                 01L
+#define SLOWED                 02L
+#define INVISIBLE              04L
+#define ASLEEP                010L
+#define WAKENS                020L
+#define WANDERS               040L
+#define FLIES                0100L
+#define FLITS                0200L
+#define CAN_FLIT             0400L /* can, but usually doesn't, flit */
+#define CONFUSED            01000L
+#define RUSTS               02000L
+#define HOLDS               04000L
+#define FREEZES            010000L
+#define STEALS_GOLD        020000L
+#define STEALS_ITEM        040000L
+#define STINGS            0100000L
+#define DRAINS_LIFE       0200000L
+#define DROPS_LEVEL       0400000L
+#define SEEKS_GOLD       01000000L
+#define FREEZING_ROGUE   02000000L
+#define RUST_VANISHED    04000000L
+#define CONFUSES        010000000L
+#define IMITATES        020000000L
+#define FLAMES          040000000L
+#define STATIONARY     0100000000L /* damage will be 1,2,3,... */
+#define NAPPING        0200000000L /* can't wake up for a while */
+#define ALREADY_MOVED  0400000000L
 
 #define SPECIAL_HIT		(RUSTS|HOLDS|FREEZES|STEALS_GOLD|STEALS_ITEM|STINGS|DRAINS_LIFE|DROPS_LEVEL)
 
@@ -392,21 +392,21 @@ extern object level_monsters;
 #define PARTY_WAKE_PERCENT 75
 
 #define HYPOTHERMIA 1
-#define STARVATION 2
+#define STARVATION  2
 #define POISON_DART 3
-#define QUIT 4
-#define WIN 5
-#define KFIRE 6
+#define QUIT        4
+#define WIN         5
+#define KFIRE       6
 
-#define UPWARD 0
-#define UPRIGHT 1
-#define RIGHT 2
+#define UPWARD    0
+#define UPRIGHT   1
+#define RIGHT     2
 #define DOWNRIGHT 3
-#define DOWN 4
-#define DOWNLEFT 5
-#define LEFT 6
-#define UPLEFT 7
-#define DIRS 8
+#define DOWN      4
+#define DOWNLEFT  5
+#define LEFT      6
+#define UPLEFT    7
+#define DIRS      8
 
 #define ROW1 7
 #define ROW2 15
